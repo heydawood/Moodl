f55a6569454652857628165d9b8c19bdbf9fca62
Dawood Faisal <dawood_faisal@yahoo.com>
2024-08-27 17:07:47 +0500

    commit-3

--- app/layout.js
+++ app/layout.js
@@ -1,7 +1,8 @@
-import { Inter } from "next/font/google";
+import { Fugaz_One, Inter } from "next/font/google";
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
+const fugaz = Fugaz_One({ subsets: ["latin"], weight: ['400'] });
 
 export const metadata = {
   title: "Moodl",
@@ -9,13 +10,26 @@ export const metadata = {
 };
 
 export default function RootLayout({ children }) {
+
+  const header = (
+    <header className="p-4 sm:p-8 flex items-center justify-between gap-4">
+      <h1 className={'text-base sm:text-lg textGradient ' + fugaz.className}>Moodl</h1>
+    </header>
+
+  )
+  const footer = (
+    <footer>
+      footer
+    </footer>
+  )
+
   return (
     <html lang="en">
-      <body className={inter.className}>
-        <header>header</header>
+      <body className={'w-full max-w-[1000px] mx-auto text-sm sm:text-base min-h-screen flex flex-col ' + inter.className}>
+        {header}
         {children}
-        <footer>footer</footer>
-        </body>
+        {footer}
+      </body>
     </html>
   );
 }
