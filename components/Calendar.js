37769bfa014f79a9f6338127c97a2e016c394a54
Dawood Faisal <dawood_faisal@yahoo.com>
2024-09-02 16:00:15 +0500

    commit-12

--- components/Calendar.js
+++ components/Calendar.js
@@ -1,11 +1,24 @@
 import React from 'react'
 
+
+const months = { 'January': 'Jan', 'February': 'Feb', 'March': 'Mar', 'April': 'Apr', 'May': 'May', 'June': 'Jun', 'July': 'Jul', 'August': 'Aug', 'September': 'Sept', 'October': 'Oct', 'November': 'Nov', 'December': 'Dec' }
+const monthsArr = Object.keys(months)
+const now = new Date()
+const dayList = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
+
 export default function Calendar() {
 
+  const year = 2024
+  const month = 'September'
+  const monthNow = new Date(year, Object.keys(months).indexOf(month), 1)
+  const firstDayOfMonth = monthNow.getDay()
+  const daysInMonth = new Date(year, Object.keys(month).indexOf(month) + 1, 0)
 
-
+  const daysToDisplay = firstDayOfMonth + daysInMonth;
+  const numRows = (Math.floor(daysToDisplay / 7)) + (daysToDisplay % 7 ? 1 : 0)
+  
   return (
-    <div>
+    <div className='flex flex-col overflow-hidden'>
 
 
 
